da86849aac8ef10433fa8f04caf1e10aeb50e916
Umarov Ramil <lilya1976.03.03@gmail.com>
2023-10-12 15:32:36 +0600

    Задание 2 - создание доски

---
+++ ex-2/board.ts
@@ -0,0 +1,28 @@
+const createDesk = (chessSize: number) => {
+    const size: number = chessSize;
+    let chess: string = '';
+
+    for (let i = 1; i <= size; i++){
+        if(i % 2 === 1) {
+            for (let j = 1; j <= size; j++){
+                if (j % 2 === 1){
+                    chess += '  ';
+                } else if(j === size) {
+                    chess += '██\n';
+                } else {
+                    chess += '██';
+                }
+            }
+        } else {
+            for (let j = 1; j <= size; j++){
+                if (j % 2 === 1){
+                    chess += '██';
+                } else if (j === size) {
+                    chess += '  \n';
+                } else {
+                    chess += '  ';
+                }
+            }
+        }
+    }
+};
