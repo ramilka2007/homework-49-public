d4596e2a37c685a4bfe5065c1380c9a639b4ccd4
Umarov Ramil <lilya1976.03.03@gmail.com>
2023-10-12 15:33:17 +0600

    Задание 1 - окончательный результат

---
+++ ex-1.sh
@@ -0,0 +1,15 @@
+mkdir projects
+cd projects
+mkdir my_project
+cd my_project
+mkdir directory1 directory2 directory3
+cd directory1
+mkdir subdirectory1
+cd -
+cd directory2
+touch hello.txt
+echo 'Hello from JS' > hello.txt
+cd -
+cd directory3
+mkdir subdirectory2
+
